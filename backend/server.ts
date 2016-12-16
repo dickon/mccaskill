7027d70696224f485a98037c6fd9f72082d45b5b
Dickon Reed <dickon.reed@citrix.com>
2016-12-16 22:38:13 +0000

    refactor: add a Zone class
    also place tslint

--- backend/server.ts
+++ backend/server.ts
@@ -17,14 +17,14 @@ interface LoginResponse {
 }
 
 interface AccountInfo {
-  userId: string,
-  username: string,
-  firstname: string,
-  lastname: string,
-  streetAddress: string,
-  city: string,
-  postcode: string,
-  country: string,
+  userId: string;
+  username: string;
+  firstname: string;
+  lastname: string;
+  streetAddress: string;
+  city: string;
+  postcode: string;
+  country: string;
   language: string;
 }
 
@@ -35,9 +35,14 @@ app.use((req, res, next) => {
   next();
 });
 
+class Zone {
+  constructor(public zoneId: string, public zoneName: string, public zoneType: string,
+              public locationId: string, public locationName: string) {
+  }
+}
 
 class HoneywellClient {
-  public zoneNames: string[] = [];
+  public zones: Zone[] = [];
   private _accessToken: string;
   private _account: AccountInfo;
   private _credentials: Credentials;
@@ -87,7 +92,7 @@ class HoneywellClient {
       headers: {
         Authorization: `bearer ${this._accessToken}`,
         applicationId : 'b013aa26-9724-4dbd-8897-048b9aada249',
-        Accept: 'application/json, application/xml, text/json, '+
+        Accept: 'application/json, application/xml, text/json, ' +
               'text/x-json, text/javascript, text/xml'
       },
       path: '/WebAPI/emea/api/v1/userAccount'},
@@ -106,21 +111,24 @@ class HoneywellClient {
       headers: {
         Authorization: `bearer ${this._accessToken}`,
         applicationId : 'b013aa26-9724-4dbd-8897-048b9aada249',
-        Accept: 'application/json, application/xml, text/json, '+
+        Accept: 'application/json, application/xml, text/json, ' +
               'text/x-json, text/javascript, text/xml'
       },
-      path: `/WebAPI/emea/api/v1/location/installationInfo?userId=${this._account.userId}&includeTemperatureControlSystems=True`},
+      path: `/WebAPI/emea/api/v1/location/installationInfo?userId=${this._account.userId}` +
+            `&includeTemperatureControlSystems=True`},
       ((res: IncomingMessage) => {
           res.setEncoding('utf8');
           res.on('data', (data: string) => {
             console.log(`get ${data}`);
             const obj = JSON.parse(data);
-            console.log(`first ${obj[0].gateways}`)
+            console.log(`first ${obj[0].gateways}`);
             for (let location of obj) {
               for (let gateway of location.gateways) {
                 for (let system of gateway.temperatureControlSystems) {
                   for (let zone of system.zones) {
-                    this.zoneNames.push(zone.name);
+                    const nzone = new Zone(zone.zoneId, zone.name, zone.zoneType,
+                                           location.locationId, location.name);
+                    this.zones.push(nzone);
                   }
                 }
               }
@@ -135,11 +143,12 @@ let hcli = (new HoneywellClient());
 hcli.login();
 
 app.get('/dickon', (req, res) => {
-  res.send(JSON.stringify(hcli.zoneNames.join()));
+  res.send(JSON.stringify(hcli.zones.map((zone: Zone) => zone.zoneName).join()));
 });
 
 app.get('/mock', (req, res) => {
-  res.send(JSON.stringify(['LivingRoom', 'MasterBed', 'Ensuite', 'DressingRoom', 'DickonStudy', 'DiningRoom', 'GuestSuite', 'Kitchen', 'Utility', 'DownOther', 'ServerRoom', 'JennyStudy']));
+  res.send(JSON.stringify(['LivingRoom', 'MasterBed', 'Ensuite', 'DressingRoom', 'DickonStudy',
+     'DiningRoom', 'GuestSuite', 'Kitchen', 'Utility', 'DownOther', 'ServerRoom', 'JennyStudy']));
 });
 
 app.listen(3001, () => console.log('started on port 3001'));
