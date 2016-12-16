d8bda1b2e7bebc6050725b1a9c1ccfc5ab7c8d43
Dickon Reed <dickon.reed@citrix.com>
2016-12-16 17:44:00 +0000

    refactor: pick up credentials from a JSON file
    add a tsconfig.json so we can pick up modules

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,7 +1,16 @@
 import * as express from 'express';
+import * as fs from 'fs';
 
 let app = express();
 
+let text = fs.readFileSync('credentials.json', 'utf8');
+interface Credentials {
+  username: string;
+  password: string;
+  deviceid: string
+}
+let credentials: Credentials= JSON.parse(text);
+console.log(`user ${credentials.username}`);
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -10,6 +19,10 @@ app.use((req, res, next) => {
 
 app.get('/mock', (req, res) => {
   res.send('DICKONSTUDY');
+});
+
+
+app.get('/dickon', (req, res) => {
 });
 
 app.listen(3001, () => console.log('started on port 3001'));
