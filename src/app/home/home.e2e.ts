60987a8d560a34c1d8944b143b0d05749bd4c8e2
Dickon Reed <dickon.reed@citrix.com>
2016-12-16 15:05:05 +0000

    red bar: add an e2e test that looks for a string that should come from my house system

--- src/app/home/home.e2e.ts
+++ src/app/home/home.e2e.ts
@@ -8,17 +8,16 @@ describe('App', () => {
   });
 
 
-  it('should have a title', () => {
+  it('should have some kind of title', () => {
     let subject = browser.getTitle();
     let result  = 'Angular2 Webpack Starter by @gdi2290 from @AngularClass';
     expect(subject).toEqual(result);
   });
 
-  it('should have `your content here` x-large', () => {
-    let subject = element(by.css('[x-large]')).getText();
-    let result  = 'Your Content Here';
-    expect(subject).toEqual(result);
+  it('should have DickonStudy in the page', () => {
+    let subject = element(by.id('dickonstudy')).getText();
+    let result  = 'DICKONSTUDY';
+    expect(subject).toContain(result);
   });
-
 
 });
