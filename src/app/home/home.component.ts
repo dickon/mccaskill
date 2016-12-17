2ca20ed9d484936e4a59213a5b87d44a08dae8c4
Dickon Reed <dickon.reed@citrix.com>
2016-12-17 00:39:31 +0000

    refactor: colour spectrum for temperatures

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,6 +5,24 @@ import { Title } from './title';
 import { XLarge } from './x-large';
 import { Observable } from 'rxjs/Rx';
 import * as d3 from 'd3';
+import * as scale from 'd3-scale';
+
+function rgbTemperature(temp: number) {
+  var l = d3.scaleLinear().domain([0,40]).range([400, 700])(temp);
+  var t = 0;
+  var r = 0, g = 0, b = 0;
+  if ((l>=400.0)&&(l<410.0)) { t=(l-400.0)/(410.0-400.0); r=    +(0.33*t)-(0.20*t*t); }
+  else if ((l>=410.0)&&(l<475.0)) { t=(l-410.0)/(475.0-410.0); r=0.14         -(0.13*t*t); }
+  else if ((l>=545.0)&&(l<595.0)) { t=(l-545.0)/(595.0-545.0); r=    +(1.98*t)-(     t*t); }
+  else if ((l>=595.0)&&(l<650.0)) { t=(l-595.0)/(650.0-595.0); r=0.98+(0.06*t)-(0.40*t*t); }
+  else if ((l>=650.0)&&(l<700.0)) { t=(l-650.0)/(700.0-650.0); r=0.65-(0.84*t)+(0.20*t*t); }
+        if ((l>=415.0)&&(l<475.0)) { t=(l-415.0)/(475.0-415.0); g=             +(0.80*t*t); }
+  else if ((l>=475.0)&&(l<590.0)) { t=(l-475.0)/(590.0-475.0); g=0.8 +(0.76*t)-(0.80*t*t); }
+  else if ((l>=585.0)&&(l<639.0)) { t=(l-585.0)/(639.0-585.0); g=0.84-(0.84*t)           ; }
+        if ((l>=400.0)&&(l<475.0)) { t=(l-400.0)/(475.0-400.0); b=    +(2.20*t)-(1.50*t*t); }
+  else if ((l>=475.0)&&(l<560.0)) { t=(l-475.0)/(560.0-475.0); b=0.7 -(     t)+(0.30*t*t); }
+  return `rgb(${Math.floor(r*256)}, ${Math.floor(g*256)}, ${Math.floor(b*256)})`;
+}
 
 @Component({
   // The selector is what angular internally uses
@@ -30,7 +48,7 @@ export class HomeComponent {
 
   ngOnInit() {
     console.log('hello `Home` component');
-    this.http.get('http://localhost:3001/mock').subscribe( (res: Response) => {
+    this.http.get('http://localhost:3001/dickon').subscribe( (res: Response) => {
       let zones = res.json();
       console.log(res.text());
       let newrows = d3.select('#zonesTable').selectAll('tr').data(zones).enter().append('tr')
@@ -39,8 +57,10 @@ export class HomeComponent {
       newrows.append('td').attr('id', (z: any) => z.zoneName + 'Temperature')
         .attr('class', 'ZoneTemperature');
       d3.select('#zonesTable').selectAll('.ZoneTemperature')
-        .data(zones).text((z: any) => z.temperature);
+        .data(zones).text((z: any) => z.temperature)
+        .attr('style', ((z:any) => 'background-color: '+rgbTemperature(z.temperature)));
       d3.select('#zonesTable').selectAll('.ZoneLabel').data(zones).text((z: any) => z.zoneName);
+
     });
     // this.title.getData().subscribe(data => this.data = data);
   }
